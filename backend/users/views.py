4408f18f5befe65943de2d26f7b23a711a455526
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-02 22:59:47 +0900

    feat: 토큰 유효성 검사 코드 추가

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 @csrf_exempt
+def validate_token(req):
+    if req.method == 'POST':
+        try:
+            pass
+        except Exception as e:
+            return JsonResponse({'valid': False, 'error': str(e)}, status=400)
+    else:
+        return JsonResponse({'error': 'POST 요청만 허용됩니다.'}, status=405)
