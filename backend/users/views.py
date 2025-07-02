b4be7405f89569f7c41af5d0298b659b43a4f916
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-02 23:06:42 +0900

    feat: token 유효성 검사 코드 추가

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from canvasapi import Canvas
 import json
 
 # Create your views here.
@@ -7,7 +8,17 @@ import json
 def validate_token(req):
     if req.method == 'POST':
         try:
-            pass
+            data = json.loads(req.body)
+            token = data.get('token')
+            if not token:
+                return JsonResponse({'valid': False, 'error': '토큰이 없습니다.'}, status=400)
+            
+            API_URL = "https://knulms.kongju.ac.kr/"
+            canvas = Canvas(API_URL, token)
+            user = canvas.get_current_user() # 유효성 검사
+
+            # 정상적인 user 정보일 경우
+            return JsonResponse({'valid': True, 'user': {'id': user.id, 'name': user.name}})
         except Exception as e:
             return JsonResponse({'valid': False, 'error': str(e)}, status=400)
     else:
