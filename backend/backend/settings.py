882f48511e7137592a70ae09f7217290c99f5f80
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-02 22:55:24 +0900

    feat: users 앱 연결

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django', # social-auth-app-django (OAuth 라이브러리)
+    'users',
 ]
 
 MIDDLEWARE = [
