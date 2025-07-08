6c53853adfafae3143d8ee40e4a74b0debfbe403
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 02:40:04 +0900

    remove: 과제, 영상, 시험 앱 제거

--- backend/lectures/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class LectureVideo(models.Model):
-    title = models.CharField(max_length=100)
-    course = models.CharField(max_length=30)
-    dueAt = models.DateTimeField()
