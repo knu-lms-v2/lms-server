27bb7e0cd18475ca166f830e9351e420509def8e
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 01:52:41 +0900

    feat: lectures, exams DB 스키마 정의

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Assignments(models.Model):
+class Assignment(models.Model):
     title = models.CharField(max_length=100)
     course = models.CharField(max_length=30)
     due_at = models.DateTimeField()
--- backend/exams/models.py
+++ backend/exams/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Exam(models.Model):
+    title = models.CharField(max_length=100)
+    course = models.CharField(max_length=30)
+    due_at = models.DateTimeField()
