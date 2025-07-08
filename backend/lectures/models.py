274e88850dedbbc4be20aa980adade693631575c
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 02:06:13 +0900

    feat: 각 앱 admin.py에 등록하여 관리자 페이지에 띄우기

--- backend/lectures/models.py
+++ backend/lectures/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class LectureVideo(models.Model):
     title = models.CharField(max_length=100)
     course = models.CharField(max_length=30)
-    due_at = models.DateTimeField()
+    dueAt = models.DateTimeField()
