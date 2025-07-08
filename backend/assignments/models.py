3bfc554537fb8d366846471defff187b06c28309
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 01:50:27 +0900

    feat: Assignments DB 스키마 정의

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Assignments(models.Model):
+    title = models.CharField(max_length=100)
+    course = models.CharField(max_length=30)
+    due_at = models.DateTimeField()
