8971b5b588b12349b6bbaf7c972dce0526dbabb4
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 01:48:01 +0900

    feat: 과제, 강의영상, 시험 앱 생성

--- backend/upcoming_list/views.py
+++ backend/upcoming_list/views.py
@@ -1,9 +1,14 @@
+from datetime import timedelta, timezone
 from django.http import JsonResponse
-from django.shortcuts import render
 
 # Create your views here.
 def upcoming(req):
     if req.method == 'GET':
-        pass
+        now = timezone.now()
+        end = now + timedelta(days=7)
+
+        # DB 필터링 부분
+
+        
     else:
         return JsonResponse()
