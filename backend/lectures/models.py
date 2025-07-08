ea403bbd8645070293002fdaa479b9432a81ef1b
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-09 01:52:41 +0900

    feat: lectures, exams DB 스키마 정의

--- backend/lectures/models.py
+++ backend/lectures/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class LectureVideo(models.Model):
+    title = models.CharField(max_length=100)
+    course = models.CharField(max_length=30)
+    due_at = models.DateTimeField()
