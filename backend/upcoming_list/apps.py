7b13f028545d12faf381e8b540fb6d97d0c102ca
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-11 16:39:35 +0900

    feat: DB에 주기적으로 저장하기 위한 스키마 정의

--- backend/upcoming_list/apps.py
+++ backend/upcoming_list/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DeadlineListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'deadline_list'
+    name = 'upcoming_list'
