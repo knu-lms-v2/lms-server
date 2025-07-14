785a4f161b20e597f216622b9fc55892f5faaee2
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-14 22:39:14 +0900

    feat: DB에 마지막 접속일 추가

--- backend/upcoming_list/models.py
+++ backend/upcoming_list/models.py
@@ -8,6 +8,7 @@ class UpcomingData(models.Model):
     week = models.CharField(max_length=50)
     remaining_days = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
+    last_accessed = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.user_name} - {self.course_name} - {self.week}"
