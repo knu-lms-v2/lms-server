03e131f499491d4ab1aa9cc7fe30192f8bf360f4
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-15 23:37:26 +0900

    feat: admin 창에 upcoming_list 띄우기

--- backend/upcoming_list/admin.py
+++ backend/upcoming_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UpcomingData
 
 # Register your models here.
+@admin.register(UpcomingData)
+class UpcomingDataAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'type', 'course_name', 'week', 'due_date', 'created_at']
+    search_fields = ['user_name', 'courese_name']
--- backend/upcoming_list/services.py
+++ backend/upcoming_list/services.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-from django.utils import timezone
+from datetime import datetime, timedelta, timezone
 import json
 import re
 from canvasapi import Canvas
@@ -35,9 +34,6 @@ def is_due_within_7_days(due_at) -> bool:
     """
     now = datetime.now(timezone.utc)
     end = now + timedelta(days=7)
-    print(f"now: {now}")
-    print(f"end: {end}")
-    print(f"remaining_days: {due_at}")
     return now <= due_at <= end
 
 def extract_week_number(week_str) -> str:
@@ -100,7 +96,8 @@ def update_user_upcoming_list(user_name):
                 if not due_at:
                     continue
 
-                if not is_due_within_7_days(due_at):
+                due_date = is_due_within_7_days(due_at)
+                if not due_date:
                     continue
 
                 week_clean = get_week_from_maps(a, assignment_week_map)
@@ -108,7 +105,7 @@ def update_user_upcoming_list(user_name):
                     'type': upcoming_type,
                     'course_name': course_name,
                     'week': week_clean,
-                    'remaining_days': due_at
+                    'due_date': due_date
                 }
                 lecture_data.append(data)
 
@@ -119,10 +116,11 @@ def update_user_upcoming_list(user_name):
                     course_name=course_name,
                     week=week_clean,
                     defaults={
-                        'remaining_days':remaining_days
+                        'due_date': due_date
                     }
                 )
-        except Exception:
+        except Exception as e:
+            print(f"error: {e}")
             continue
     return lecture_data
     
